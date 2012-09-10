0b96d44e21fc1472beaf2d5ee1fbd67d2924ad13
KOMATSU Seiji <skomatsu.comutt@gmail.com>
2012-09-10 15:04:18 +0900

    Modified runner

--- 02_ci/README.md
+++ 02_ci/README.md
@@ -1,6 +1,14 @@
 Jenkins で CI しよう
 =====================
 
+Git plugin を Jenkins に入れる
+
+Mange Jenkins -> Configure で、
+以下の項目を設定する
+
+* Git plugin
+    * Global Config user.name Value
+    * Global Config user.email Value
 
 https://github.com/comutt/unittest-study.git を参照する Free style project を作る
 (branch: b_ci)
@@ -8,10 +16,10 @@ https://github.com/comutt/unittest-study.git を参照する Free style project 
 
 以下の内容で、 "Execute shell" ステップを登録する
 
-unittest-study/02_ci/bin/envjs.runner.sh unittest-study/02_ci/junit_xml_reporter.html
+02_ci/bin/envjs.runner.sh unittest-study/02_ci/junit_xml_reporter.html
 
 Windows の場合は、 Cygwin を入れた上で、
 以下の内容で "Execute windows batch command" を登録する。
 
-bash -c "unittest-study/02_ci/bin/envjs.runner.sh unittest-study/02_ci/junit_xml_reporter.html"
+bash -c "02_ci/bin/envjs.runner.sh unittest-study/02_ci/junit_xml_reporter.html"
 
--- 02_ci/bin/envjs.runner.sh
+++ 02_ci/bin/envjs.runner.sh
@@ -13,15 +13,12 @@ if [ "${os}" = "Cygwin" ]; then
     CLASSPATH=$(cygpath -w "${EXTLIB_DIR}/js.jar")
     CLASSPATH="${CLASSPATH};$(cygpath -w ${EXTLIB_DIR}/jline.jar)"
     ROOTDIR_FILEURL="file:///$(cygpath -w ${ROOTDIR_FULLPATH} | sed -e 's,\\,/,g')"
-    FILES=''
-    for f in $@; do
-       dir=$(cd $(dirname ${f}) && pwd)
-       FILES="${FILES} file:///$(cygpath -w ${dir}/$(basename ${f}) | sed -e 's,\\,/,g')"
-    done
+    dir=$(cd $(dirname ${f}) && pwd)
+    TARGET="file:///$(cygpath -w ${dir}/$(basename $1) | sed -e 's,\\,/,g')"
 else
     CLASSPATH=${EXTLIB_DIR}/js.jar:${EXTLIB_DIR}/jline.jar
     ROOTDIR_FILEURL="file:///${ROOTDIR_FULLPATH}"
-    FILES="$@"
+    TARGET=$1
 fi
 
-java -cp "${CLASSPATH}" org.mozilla.javascript.tools.shell.Main -opt -1 ${LIB_DIR}/envjs.bootstrap.js ${ROOTDIR_FILEURL} $FILES
+java -cp "${CLASSPATH}" org.mozilla.javascript.tools.shell.Main -opt -1 ${LIB_DIR}/envjs.bootstrap.js ${ROOTDIR_FILEURL} "${TARGET}"
