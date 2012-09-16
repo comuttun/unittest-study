116a2d995871207f917c7442ee154956bc196a94
KOMATSU Seiji <skomatsu.comutt@gmail.com>
2012-09-17 01:31:54 +0900

    Modified runner, windows batch version (final)

--- ci/bin/envjs.runner.bat
+++ ci/bin/envjs.runner.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set CWD=%~dp0
-echo %CWD%
-set ROOT_DIR=%CWD%..
+set MYDIR=%~dp0
+echo %MYDIR%
+set ROOT_DIR=%MYDIR%..
 set EXTLIB_DIR=%ROOT_DIR%\extlib
 set LIB_DIR=%ROOT_DIR%\lib
 set BOOTSTRAP_JS=%ROOT_DIR%\envjs.bootstrap.js
@@ -12,14 +12,15 @@ REM Uncomment if you wanna to override JAVA_HOME
 REM set JAVA_HOME=
 
 REM Change the following to match your environment
-if "%JAVA_HOME%" == "" (
-    set JAVA_HOME="C:\Program Files (x86)\Java\jdk1.6.0_35"
-)
+if "%JAVA_HOME%" == "" set JAVA_HOME=C:\Program Files (x86)\Java\jdk1.6.0_35
 
 del /q TEST-*.xml
 
 set CLASSPATH="%EXTLIB_DIR%\js.jar"
-set JAVA=%JAVA_HOME%\bin\java
-set BOOTSTRAP_JS=%LIB_DIR%\envjs.bootstrap.js
-echo %JAVA% -cp %CLASSPATH% %MAIN_CLASS% -opt -1 "%BOOTSTRAP_JS%" "%ROOT_DIR%" %*
-%JAVA% -cp %CLASSPATH% %MAIN_CLASS% -opt -1 "%BOOTSTRAP_JS%" "%ROOT_DIR%" %*
+set JAVA=%JAVA_HOME:"=%\bin\java
+set JAVA="%JAVA%"
+set BOOTSTRAP_JS="%LIB_DIR%\envjs.bootstrap.js"
+set ROOT_DIR="%ROOT_DIR%"
+
+echo %JAVA% -cp %CLASSPATH% %MAIN_CLASS% -opt -1 %BOOTSTRAP_JS% %ROOT_DIR% %*
+%JAVA% -cp %CLASSPATH% %MAIN_CLASS% -opt -1 %BOOTSTRAP_JS% %ROOT_DIR% %*
