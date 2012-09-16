df1a431bb03ef8e86a03f1385d35648497b8b975
KOMATSU Seiji <skomatsu.comutt@gmail.com>
2012-09-16 21:36:17 +0900

    Modified a runner, windows batch command version

--- ci/bin/envjs.runner.bat
+++ ci/bin/envjs.runner.bat
@@ -2,7 +2,7 @@
 
 set CWD=%~dp0
 echo %CWD%
-set ROOT_DIR="%CWD%.."
+set ROOT_DIR=%CWD%..
 set EXTLIB_DIR=%ROOT_DIR%\extlib
 set LIB_DIR=%ROOT_DIR%\lib
 set BOOTSTRAP_JS=%ROOT_DIR%\envjs.bootstrap.js
@@ -13,13 +13,13 @@ REM set JAVA_HOME=
 
 REM Change the following to match your environment
 if "%JAVA_HOME%" == "" (
-    set JAVA_HOME="C:\\Program Files (x86)\\Java\\jdk1.6.0_35"
+    set JAVA_HOME=C:\\Program Files (x86)\\Java\\jdk1.6.0_35
 )
 
 del /q TEST-*.xml
 
-set CLASSPATH=%EXTLIB_DIR%\js.jar
-set JAVA="%JAVA_HOME%"\bin\java
+set CLASSPATH="%EXTLIB_DIR%\js.jar"
+set JAVA=%JAVA_HOME%\bin\java
 set BOOTSTRAP_JS=%LIB_DIR%\envjs.bootstrap.js
-echo %JAVA% -cp %CLASSPATH% %MAIN_CLASS% -opt -1 %BOOTSTRAP_JS% %ROOT_DIR% %*
-%JAVA% -cp %CLASSPATH% %MAIN_CLASS% -opt -1 %BOOTSTRAP_JS% %ROOT_DIR% %*
+echo "%JAVA%" -cp %CLASSPATH% %MAIN_CLASS% -opt -1 "%BOOTSTRAP_JS%" "%ROOT_DIR%" %*
+"%JAVA%" -cp %CLASSPATH% %MAIN_CLASS% -opt -1 "%BOOTSTRAP_JS%" "%ROOT_DIR%" %*
